3423853f2ee657defc0d920e3dfa0d3153c85d24
123Joy123 <2506114287@qq.com>
2021-05-06 21:38:59 +0800

    树的bfs和dfs

---
+++ tree/广度优先遍历bfs.js
@@ -0,0 +1,46 @@
+const tree={
+    val:'a',
+    children:[
+        {
+            val:'b',
+            children:[
+                {
+                    val:'d',
+                    children:[],
+                },
+                {
+                    val:'e',
+                    children:[],
+                }
+            ],
+        },
+        {
+            val:'c',
+            children:[
+                {
+                    val:'f',
+                    children:[],
+                },
+                {
+                    val:'g',
+                    children:[],
+                }
+            ],
+        }
+    ],
+}
+
+const bfs=(root)=>{
+    const q=[root]//初始化一个队列，并把root入队
+    //队头出队并访问
+    //把队头的children都入队
+    while(q.length>0){
+        const n=q.shift()
+        console.log(n.val)
+        n.children.forEach(child=>{
+            q.push(child)
+        })
+    }
+}
+
+bfs(tree)
